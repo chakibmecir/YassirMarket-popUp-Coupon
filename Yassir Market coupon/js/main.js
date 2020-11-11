0870c5702cb686a3244c8e0323511b96e375898e
chakibmecir <65046793+chakibmecir@users.noreply.github.com>
2020-11-11 14:56:56 +0100

    Add files via upload

---
+++ Yassir Market coupon/js/main.js
@@ -0,0 +1,34 @@
+//pop show
+const toTop = document.querySelector(".coupon-pop");
+
+    window.addEventListener("scroll", ()=>{
+        if (window.pageYOffset > 50){
+            toTop.classList.add("active");
+        }
+    })
+
+//close pop
+var menu = document.getElementById("popout");
+function closeCoupon() {
+    popout.style.opacity = 0;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Unix timestamp (in seconds) to count down to
+    var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+  
+    // Set up FlipDown
+    var flipdown = new FlipDown(twoDaysFromNow)
+  
+      // Start the countdown
+      .start()
+  
+      // Do something when the countdown ends
+      .ifEnded(() => {
+        console.log('The countdown has ended!');
+      });
+  
+  });
